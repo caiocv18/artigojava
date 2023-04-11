dfb28b7dc68f770a60fc77fc0b0cc6da4af7e9e9
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-04-11 01:40:54 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/caiocv18/artigojava/ArtigojavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.caiocv18.artigojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtigojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtigojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caiocv18/artigojava/ArtigojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caiocv18.artigojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtigojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
