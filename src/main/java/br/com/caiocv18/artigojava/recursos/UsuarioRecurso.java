82ee17be96b0f3292ea74a883a866409f3a18221
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-04-11 16:26:17 -0300

    Pegando os usuários do banco e adicionando a pesquisa por Id

--- src/main/java/br/com/caiocv18/artigojava/recursos/UsuarioRecurso.java
+++ src/main/java/br/com/caiocv18/artigojava/recursos/UsuarioRecurso.java
@@ -1,18 +1,30 @@
 package br.com.caiocv18.artigojava.recursos;
 
 import br.com.caiocv18.artigojava.entidades.Usuario;
+import br.com.caiocv18.artigojava.servicos.UsuarioServico;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioRecurso {
+    @Autowired
+    UsuarioServico servico;
     @GetMapping
-    public ResponseEntity<Usuario> procurarTodos(){
+    public ResponseEntity<List<Usuario>> procurarTodos(){
+        List<Usuario> lista = servico.pesquisarTodos();
+        return ResponseEntity.ok().body(lista);
+    }
 
-        Usuario novoUsuario = new Usuario(1, "Caio", "caiocv18_dev@gmail.com", "12345");
-        return ResponseEntity.ok().body(novoUsuario);
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Usuario> procurarTodos(@PathVariable Long id){
+        Usuario usuario = servico.procurarPorId(id);
+        return ResponseEntity.ok().body(usuario);
     }
 }
---
+++ src/main/java/br/com/caiocv18/artigojava/servicos/UsuarioServico.java
@@ -0,0 +1,24 @@
+package br.com.caiocv18.artigojava.servicos;
+
+import br.com.caiocv18.artigojava.entidades.Usuario;
+import br.com.caiocv18.artigojava.repositorios.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServico {
+    @Autowired
+    private UsuarioRepositorio repositorio;
+
+    public List<Usuario> pesquisarTodos(){
+        return repositorio.findAll();
+    }
+
+    public Usuario procurarPorId(Long id){
+        Optional<Usuario> usuario = repositorio.findById(id);
+        return usuario.get();
+    }
+}
